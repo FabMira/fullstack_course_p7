76d1164840d51b301370d87fff21b19a0c02572a
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-07-07 16:02:23 -0400

    creation form simplified w/custom hook

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { use } from 'react'
 import { useState } from 'react'
+import { useField } from './hooks'
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate, useParams } from 'react-router-dom'
 
 const Menu = () => {
@@ -74,17 +75,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author  = useField('text')
+  const info    = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -96,15 +97,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
