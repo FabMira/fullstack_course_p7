54761527f7b7cbc6007f82813ecefd1a8d182b8f
Fabrizio Mirabella <fmirabella08@gmail.com>
2025-07-23 00:30:49 -0400

    finished 7.17  nav bar

--- bloglist-frontend/src/components/Menu.jsx
+++ bloglist-frontend/src/components/Menu.jsx
@@ -1,6 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Button, Nav } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { setUser } from "../reducers/userReducer";
+import blogService from "../services/blogs";
 
 const Menu = () => {
   const location = useLocation();
@@ -9,18 +12,19 @@ const Menu = () => {
     paddingRight: 5,
     textDecoration: "none",
   };
+  const dispatch = useDispatch();
 
   return (
     <Nav variant="pills" defaultActiveKey="/" activeKey={location.pathname}>
       <Nav.Item>
-        <Link to="/" style={padding}>
+        <Link to="/" style={padding} className="nav-link">
           <Nav.Link href="/" as="span">
             Blogs
           </Nav.Link>
         </Link>
       </Nav.Item>
       <Nav.Item>
-        <Link to="/users" style={padding}>
+        <Link to="/users" style={padding} className="nav-link">
           <Nav.Link href="/users" as="span">
             Users
           </Nav.Link>
@@ -29,16 +33,28 @@ const Menu = () => {
       <Nav.Item>
         {user ? (
           <div>
-            <em>{user.name} logged in</em>
+            <em style={{ paddingRight: 10 }}>{user.name} logged in</em>
           </div>
         ) : (
-          <Link to="/login" style={padding}>
+          <Link to="/login" style={padding} className="nav-link">
             <Nav.Link href="/login" as="span">
               Login
             </Nav.Link>
           </Link>
         )}
       </Nav.Item>
+      {user && (
+        <Button
+          variant="danger"
+          onClick={() => {
+            window.localStorage.removeItem("loggedBlogAppUser");
+            dispatch(setUser(null));
+            blogService.setToken(null);
+          }}
+        >
+          Logout
+        </Button>
+      )}
     </Nav>
   );
 };
